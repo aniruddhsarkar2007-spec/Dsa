62318f1a06f3f97f93ece43f9d68fb1d35344e55
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-16 12:41:49 +0530

    Dsa with C++

---
+++ arrayday1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // syntax of 1D 
+    // datatype arrayname [size];
+    // subscript=[size]
+
+int a[3];
+a[0]=23;
+a[1]=32;
+a[3]=43;
+cout<<a; // this will show the adress of 0 index
+
+}
