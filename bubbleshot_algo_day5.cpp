d8fed519c051a9d6c39e9224d0963dff0d097f3f
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-31 12:35:19 +0530

    DSA with c++

--- bubbleshot_algo_day5.cpp
+++ bubbleshot_algo_day5.cpp
@@ -77,6 +77,52 @@
 
 // }
 
+// its time complexity is O(n2)   
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   int s;
+//   cout<<"Enter the size of array:\n";
+//   cin>>s;
+//   int arr[s];
+//   int temp;
+//   cout<<"Enter "<<s<<" Values:\n";
+//   for(int i=0;i<s;i++)
+//   {
+//     cin>>arr[i];
+//   }
+//   cout<<"Orignal Array:\n ";
+//   for(int i=0;i<s;i++)
+//   {
+//     cout<<arr[i]<<"\n";
+//   }
+//   cout<<"Sorted array:\t";
+//   for(int i=0;i<s;i++)
+//   {
+//     for(int j=0;j<s-1;j++)
+//     {
+//         if(arr[j]>arr[j+1])
+//         {
+//             temp=arr[i];
+//             arr[i]=arr[j+1];
+//             arr[j+1]=temp;
+//         }
+//     }
+//   }
+//   for(int i=0;i<s;i++)
+//   {
+//     cout<<arr[i]<<"\t";
+//   }
+
+// }
+
+
+
+//----------31/3/26-------
+
+// optimized code
+
 
 #include<iostream>
 using namespace std;
@@ -85,6 +131,7 @@ int main()
   int s;
   cout<<"Enter the size of array:\n";
   cin>>s;
+  int c=0;
   int arr[s];
   int temp;
   cout<<"Enter "<<s<<" Values:\n";
@@ -92,7 +139,7 @@ int main()
   {
     cin>>arr[i];
   }
-  cout<<"Orignal Array:\n";
+  cout<<"Orignal Array:\n ";
   for(int i=0;i<s;i++)
   {
     cout<<arr[i]<<"\n";
@@ -100,7 +147,7 @@ int main()
   cout<<"Sorted array:\t";
   for(int i=0;i<s;i++)
   {
-    for(int j=0;j<s;j++)
+    for(int j=0;j<s-1-i;j++)
     {
         if(arr[j]>arr[j+1])
         {
@@ -108,11 +155,24 @@ int main()
             arr[i]=arr[j+1];
             arr[j+1]=temp;
         }
+        c++;
     }
   }
-  for(int i=0;i<s;i++)
-  {
+  for(int i=0;i<s;i++){
     cout<<arr[i]<<"\t";
   }
 
+  cout<<"Number of iteration:"<<c;
+
 }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int arr[]={5,3,54,65};
+//   int s=sizeof(arr)/sizeof(arr[0]);
+//   for (int i=0;i<s;i++){
+//     cout<<arr[i]<<endl;
+//   }
+// }
